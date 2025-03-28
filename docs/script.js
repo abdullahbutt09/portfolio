1770c0fc900925e36b444c080e8166b98eb6d768
alpha <abdullah777butt@gmail.com>
2025-03-28 11:33:47 +0500

    making project section responsive

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuBtn = document.getElementById("menu-btn");
-    const mobileMenu = document.getElementById("mobile-menu");
-  
-    menuBtn.addEventListener("click", function () {
-      console.log("Hamburger clicked");
-      mobileMenu.classList.toggle("hidden");
-      // Also toggle flex if needed:
-      mobileMenu.classList.toggle("flex");
-    });
+  const menuBtn = document.getElementById("menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  menuBtn.addEventListener("click", function () {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
   });
-  
+});
